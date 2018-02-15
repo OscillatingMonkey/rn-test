4174c5d6c70e4fa3cc7b76679fddb8e3842f0339
Mohammad Bashir <mbashir@theworkinggroup.ca>
2018-02-15 15:44:11 -0500

    adf

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import { AppRegistry } from 'react-native'
+import App from './App'
 
-AppRegistry.registerComponent('killi_app', () => App);
+AppRegistry.registerComponent('killi_app', () => App)
