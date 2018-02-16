9b2d1c6f923bd788499e8bd3c090aa5641e7a00d
Mohammad Bashir <mbashir@theworkinggroup.ca>
2018-02-15 19:44:13 -0500

    wip

--- android/app/src/main/java/com/killi_app/MainApplication.java
+++ android/app/src/main/java/com/killi_app/MainApplication.java
@@ -3,6 +3,7 @@ package com.killi_app;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.smixx.reactnativeicons.ReactNativeIcons;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.microsoft.appcenter.reactnative.crashes.AppCenterReactNativeCrashesPackage;
 import com.microsoft.appcenter.reactnative.analytics.AppCenterReactNativeAnalyticsPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeIcons(),
             new VectorIconsPackage(),
             new AppCenterReactNativeCrashesPackage(MainApplication.this, getResources().getString(R.string.appcenterCrashes_whenToSendCrashes)),
             new AppCenterReactNativeAnalyticsPackage(MainApplication.this, getResources().getString(R.string.appcenterAnalytics_whenToEnableAnalytics)),
